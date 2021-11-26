e8b4023b227fcd761683e94e8c9ecdfbbf86d163
Adamantiun <40891416+Adamantiun@users.noreply.github.com>
2021-11-26 15:09:36 +0000

    Acabei 11.6 do pdf

--- src/test/groovy/FirstSpecification.groovy
+++ src/test/groovy/FirstSpecification.groovy
@@ -1,12 +1,31 @@
+import com.aor.numbers.GenericListDeduplicator
 import spock.lang.Specification
 
 class FirstSpecification extends Specification {
-    def "Should be able to remove from list"() {
+    def "numbers to the power of two"(int a, int b, int c) {
+        expect:
+        Math.pow(a, b) == c
+        where:
+        a | b | c
+        1 | 2 | 1
+        2 | 2 | 4
+        3 | 2 | 9
+    }
+    def "distinct_bug_8726"(){
         given:
-        def list = [1, 2, 3, 4]
+        GenericListDeduplicator deduplicator = Mock(GenericListDeduplicator)
+        deduplicator.deduplicate(_) >> Arrays.asList(1, 2, 4)
         when:
-        list.remove(0)
+        def result = deduplicator.deduplicate(Arrays.asList(1, 2, 4, 2))
         then:
-        list == [2, 3, 4]
+        result == Arrays.asList(1,2,4)
+    }
+    def "Should verify notify was called"() {
+        given:
+        def notifier = Mock(Notifier)
+        when:
+        notifier.notify('foo')
+        then:
+        2 * notifier.notify('foo')
     }
 }

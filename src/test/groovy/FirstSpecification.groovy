73076d35e63112a09b329dcc97ee9fc12c0afbdf
Adamantiun <40891416+Adamantiun@users.noreply.github.com>
2021-11-26 14:47:05 +0000

    testing Groovy (11.3)

--- src/test/groovy/FirstSpecification.groovy
+++ src/test/groovy/FirstSpecification.groovy
@@ -1,4 +1,12 @@
 import spock.lang.Specification
 
 class FirstSpecification extends Specification {
+    def "Should be able to remove from list"() {
+        given:
+        def list = [1, 2, 3, 4]
+        when:
+        list.remove(0)
+        then:
+        list == [2, 3, 4]
+    }
 }
